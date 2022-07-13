f999c7c2b22434ae15308b91a2ce3f029a504017
shangjuc <shangjuc@gmail.com>
2022-07-13 14:13:54 +0800

    Add Navbar, Timer, LeafletMap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './component/Navbar/Navbar.jsx';
+import Timer from './component/Timer/Timer.jsx';
+import LeafletMap from './component/LeafletMap/LeafletMap.jsx';
+import './App.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Timer />} />
+        <Route path="/Timer" element={<Timer />} />
+        <Route path="/LeafletMap" element={<LeafletMap />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './Navbar.scss';
+import NavDropButton from './NavDropButton.jsx';
+
+function Navbar(props) {
+    const [nav_index, setNavIdx] = useState('');
+    
+    function click_navbtn(e){
+        let nav_value = e.target.getAttribute("nav_value")
+        if (nav_index !== nav_value){
+            setNavIdx(nav_value);
+            // window.location.href = "/" + nav_value;
+        } else {
+            setNavIdx('');
+        }
+    } 
+    function click_navdropbtn(e){
+        let nav_value = e.target.getAttribute("nav_value")
+        if (nav_index !== nav_value){
+            setNavIdx(nav_value);
+            // window.location.href = "/" + nav_value;
+
+        } else {
+            setNavIdx('');
+        }
+    } 
+    const nav_btn_arr = ['Timer', 'LeafletMap'];
+    const nav_btn_arr2 = [];
+
+    return (
+        <div id="navbar">
+            <img className="App-logo" src={logo} alt="logo"></img>
+            <ul nav_index={nav_index}>   
+                {nav_btn_arr.map(val =>
+                    <li key={val} className={'nav-btn unselectable '} onClick={click_navbtn} nav_value={val} >
+                        <Link className="nav-link" to={"/"+val}>
+                            {val}
+                        </Link>
+                    </li>)}
+                
+                {nav_btn_arr2.map(val => 
+                    <NavDropButton key={val} nav_value={val} click_navbtn={click_navdropbtn} nav_index={nav_index} />)}
+            </ul>
+            {/* <input type="file"></input> */}
+        </div>
+    );
+}
+
+export default Navbar;
